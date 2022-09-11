c638abb1a367be52a31a7ec5103c5e79173f4eb8
WilliamChen0811 <105633569+WilliamChen0811@users.noreply.github.com>
2022-09-12 00:01:58 +0800

    Add files via upload

--- js/indexscript.js
+++ js/indexscript.js
@@ -4,11 +4,33 @@ function Test(){
 	var empty_div = document.getElementById("empty_div");
 	var homeParent = home.parentNode;
 	var rightbar = document.getElementById("rightbar");
+	var gototop = document.getElementById("gototop");
 	var new_div = document.getElementById("new_div");
 	
     home.onclick = null;
 	welcome.remove();
 	empty_div.remove();
 	home.style.backgroundImage = 'url(images/door.gif)';
-	setTimeout(function(){homeParent.removeChild(home); rightbar.style.display = 'block'; new_div.style.display = 'block';},8000);
+	setTimeout(function(){homeParent.removeChild(home); rightbar.style.display = 'flex'; gototop.style.display = 'block'; new_div.style.display = 'block';},1000);
+}
+
+var FourLeafCloverZCVar;
+function gototop(){
+    FourLeafCloverZCVar=setInterval(FourLeafCloverZCEachScrollBy,10);    
+}
+function FourLeafCloverZCEachScrollBy(eachHeight){
+    if(document.documentElement && document.documentElement.scrollTop)  //IE
+    {
+        if(document.documentElement.scrollTop<=0){
+            clearInterval(FourLeafCloverZCVar);
+        }else{
+            window.scrollBy(0,-30);
+        }
+    }else{          //Chrome不支持documentElement.scrollTop
+        if(document.body.scrollTop<=0){
+            clearInterval(FourLeafCloverZCVar);
+        }else{
+            window.scrollBy(0,-30);
+        }
+    }
 }
