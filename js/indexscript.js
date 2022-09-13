4eb6d3a33d7dc2a58d386f7253a16c0a1ec6e2d0
SkaiYo <113281141+SkaiYo@users.noreply.github.com>
2022-09-14 00:50:12 +0800

    Update indexscript.js

--- js/indexscript.js
+++ js/indexscript.js
@@ -5,13 +5,13 @@ function Test(){
 	var homeParent = home.parentNode;
 	var rightbar = document.getElementById("rightbar");
 	var gototop = document.getElementById("gototop");
-	var new_div = document.getElementById("new_div");
+	var Screen1 = document.getElementById("Screen1");
 	
     home.onclick = null;
 	welcome.remove();
 	empty_div.remove();
 	home.style.backgroundImage = 'url(images/door.gif)';
-	setTimeout(function(){homeParent.removeChild(home); rightbar.style.display = 'flex'; gototop.style.display = 'block'; new_div.style.display = 'block';},1000);
+	setTimeout(function(){homeParent.removeChild(home); rightbar.style.display = 'flex'; gototop.style.display = 'block'; Screen1.style.display = 'block';},1000);
 }
 
 var FourLeafCloverZCVar;
@@ -35,9 +35,12 @@ function FourLeafCloverZCEachScrollBy(eachHeight){
     }
 }
 
-//假設DIV2是概念設計，gototop2函式就是跳到概念設計用的
-var tt = document.getElementById("DIV2");
-function gototop2(){
+//假設Screen2是概念設計，gototop2函式就是跳到概念設計用的
+function clickto(s){
+	jumpto(document.getElementById("Screen"+s))
+}
+
+function jumpto(tt){
     window.scrollTo({
 		top: (jQuery(tt).offset().top)-75,
 		behavior: "smooth"
